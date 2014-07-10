36e8693168b99e242a42c9bab1e006626cf7a7c3
davidsonalencar <davidson.t.i@gmail.com>
2014-07-10 08:44:17 -0300

    issue #12

---
+++ config/components.php
@@ -0,0 +1,48 @@
+<?php
+
+$db = require(__DIR__ . '/db.php');
+
+return [
+    'urlManager' => [
+        'class' => 'yii\web\UrlManager',
+        'enablePrettyUrl' => true,
+        'showScriptName' => false,
+    ],
+    'cache' => [
+        'class' => 'yii\caching\FileCache',
+    ],
+    'user' => [
+        'identityClass' => 'app\models\Operador',
+        'enableAutoLogin' => true,
+    ],
+    'i18n' => [
+        'translations' => [
+            'app*' => [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'basePath' => '@app/messages',
+                'sourceLanguage' => 'en-US',
+                'fileMap' => [
+                    'app' => 'app.php',
+                    'app/error' => 'error.php',
+                ],
+            ],
+        ],
+    ],
+    'errorHandler' => [
+        'errorAction' => 'site/error',
+    ],
+    'mail' => [
+        'class' => 'yii\swiftmailer\Mailer',
+        'useFileTransport' => true,
+    ],
+    'log' => [
+        'traceLevel' => YII_DEBUG ? 3 : 0,
+        'targets' => [
+            [
+                'class' => 'yii\log\FileTarget',
+                'levels' => ['error', 'warning'],
+            ],
+        ],
+    ],
+    'db' => $db,
+];
--- config/web.php
+++ config/web.php
@@ -1,7 +1,7 @@
 <?php
 
 $params = require(__DIR__ . '/params.php');
-$db = require(__DIR__ . '/db.php');
+$components = require(__DIR__ . '/components.php');
 
 $config = [
     'id' => 'basic',
@@ -9,50 +9,7 @@ $config = [
     'bootstrap' => ['log'],
     'extensions' => require(__DIR__ . '/../vendor/yiisoft/extensions.php'),
     'timezone' => 'America/Sao_Paulo',
-    'components' => [
-        'urlManager' => [
-            'class' => 'yii\web\UrlManager',
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-        ],
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'user' => [
-            'identityClass' => 'app\models\Operador',
-            'enableAutoLogin' => true,
-        ],
-        'i18n' => [
-            'translations' => [
-                'app*' => [
-                    'class' => 'yii\i18n\PhpMessageSource',
-                    'basePath' => '@app/messages',
-                    'sourceLanguage' => 'en-US',
-                    'fileMap' => [
-                        'app' => 'app.php',
-                        'app/error' => 'error.php',
-                    ],
-                ],
-            ],
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        'mail' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'useFileTransport' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'db' => $db,
-    ],
+    'components' => $components,
     'params' => $params,
 ];
 
