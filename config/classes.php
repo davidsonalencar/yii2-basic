835b192ddd21afc4778ccd90956ad79ac1ba1318
macbookpro <macbookpro@192.168.0.5>
2014-07-17 22:27:50 -0300

    Fixed #11. A configuração será utilizada apenas nas classes Helpers.

--- config/classes.php
+++ config/classes.php
@@ -1,11 +1,6 @@
 <?php
 
 /**
- * @package yii\web
- */
-//Yii::$classMap['yii\web\Controller'] = '@app/components/yii/web/Controller.php';
-
-/**
  * @package yii\helpers
  */
 Yii::$classMap['yii\helpers\VarDumper'] = '@app/components/yii/helpers/VarDumper.php';
