e05c04dc588a3a91f3abce5d5d592c8081a63dab
Davidson Alencar <davidson.t.i@gmail.com>
2014-08-17 21:35:33 -0300

    Admin #8

--- web/teste/sidebar.collapse.init.js
+++ web/teste/sidebar.collapse.init.js
@@ -1,23 +1,33 @@
+/**
+ * Faz com que apenas um grupo do menu fique aberto
+ * 
+ * @param jQuery $
+ * @returns {undefined}
+ */
 (function($) {
-    $('ul.collapse').on('show.bs.collapse', function(e) {
-        e.stopPropagation();
+    $('ul.collapse')
+            .on('show.bs.collapse', function(e) {
+                e.stopPropagation();
 
-        if ($(this).closest('#menu').length) {
-            var t = $(this).parents('.hasSubmenu').length;
-            if (t != 1)
-                return;
+                if ($(this).closest('#menu').length) {
+                    var t = $(this).parents('li').length;
+                    if (t != 1)
+                        return;
 
-            var a = $('#menu > div > div > ul > li.hasSubmenu.active > ul').not(this);
+                    var a = $('#menu > div > div > ul > li.active > ul').not(this);
 
-            a.removeClass('in').addClass('collapse').removeAttr('style')
-             .closest('.hasSubmenu.active').removeClass('active');
-        }
-    })
-    .on('shown.bs.collapse', function(e) {
-        e.stopPropagation();
+                    a.removeClass('in')
+                     .addClass('collapse')
+                     .removeAttr('style')
+                     .closest('.active')
+                     .removeClass('active');
+                }
+            })
+            .on('shown.bs.collapse', function(e) {
+                e.stopPropagation();
 
-        if ($(this).closest('#menu').length) {
-            $('#menu *').getNiceScroll().resize();
-        }
-    });
+                if ($(this).closest('#menu').length) {
+                    $('#menu *').getNiceScroll().resize();
+                }
+            });
 })(jQuery);
