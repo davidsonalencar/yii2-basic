1856aecbcf6283da3869be7f8a8bf55a6e246087
Davidson Alencar <davidson.t.i@gmail.com>
2015-01-23 16:57:30 -0200

    Atualização da pasta de testes

--- tests/codeception/config/config.php
+++ tests/codeception/config/config.php
@@ -5,7 +5,7 @@
 return [
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=127.0.0.1;dbname=yii2basic_tests',
+            'dsn' => 'mysql:host=localhost;dbname=yii2_basic_tests',
         ],
         'mailer' => [
             'useFileTransport' => true,
